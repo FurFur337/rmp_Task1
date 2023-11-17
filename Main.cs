2c4539ebd0fe0d871f074ceb461b34ef399f880d
FurFur <dedugan337@gmail.com>
2023-11-17 20:42:40 +0200

    Client added

---
+++ Main.cs
@@ -0,0 +1,12 @@
+﻿using RAGE;
+using RAGE.Elements;
+
+public class Client : Events.Script
+{
+    public Client()
+    {
+        Events.Add("SERVER:CLIENT::OnWantedLevelUpdate", WantedSystem.OnWantedLevelUpdate);
+        Events.Add("SERVER:CLIENT::OnBlipSetupForCop", WantedSystem.OnBlipSetupForCop);
+        Events.Add("SERVER:CLIENT::OnBlipRemoveForCop", WantedSystem.OnBlipRemoveForCop);
+    }
+}
---
+++ WantedSystem.cs
@@ -0,0 +1,37 @@
+﻿using RAGE;
+using RAGE.Elements;
+using RAGE.Game;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+class WantedSystem
+{
+    private static Dictionary<string, Blip> _blips = new Dictionary<string, Blip>();
+
+    public static void OnWantedLevelUpdate(object[] args)
+    {
+        int level = (int)args[0];
+
+        Misc.SetFakeWantedLevel(level);
+    }
+
+    public static void OnBlipSetupForCop(object[] args)
+    {
+        var name = (string)args[0];
+        float x = (float)args[1];
+        float y = (float)args[2];
+        float z = (float)args[3];
+
+        _blips[name] = new Blip(801, new Vector3(x, y, z), "Target", 2.0f, 1, 255, 0, false, 0, 0, 0);
+        _blips[name].SetScale(2.0f);
+    }
+
+    public static void OnBlipRemoveForCop(object[] args)
+    {
+        var name = (string)args[0];
+
+        _blips[name]?.Destroy();
+    }
+}
